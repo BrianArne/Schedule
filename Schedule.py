c56cf72b98bb813bc000b611087f3733be2897ec
Brian Arne <baarne@g.cofc.edu>
2018-12-04 14:09:43 -0500

    GERM_ method with OS implementation

--- Schedule.py
+++ Schedule.py
@@ -1,20 +1,11 @@
-# New schedule algorithm for OS managing Accelerator devices
-
-# Need checker for payload > or < allotted
-# Hash Map checking for allotted for 
-# Track time something gets in OS or GPU
-# Track time it would have gotten done
-# Track time it got done
-# Jobs processed
-
+import random
 from enum import Enum
 from random import randint
-import random
 
 #MACROS
-batch_size = 2
-cpu_time_multiplier = 2
-curr_id = 1
+GPU_QUANTUM = 2
+CURR_ID = 0
+TIME = 0
 
 
 '''
@@ -25,6 +16,7 @@ class JobSize(Enum):
     MEDIUM =2 
     LARGE = 3
     XLARGE = 4
+    XXLarge = 5
 
 '''
 Container Class represent a task. Holds ID and JobSize
@@ -46,11 +38,20 @@ class Task:
 '''
 Takes in a job and puts it in GPU or OS Queue
 '''
-def assign_queue(task):
+def assign_two_queue(task):
     if(task._size is JobSize.SMALL):
         cpu_queue.append(task)
     else:
         gpu_queue.append(task)
+# End assign_two_queue();
+
+'''
+Takes in a job and puts it in GPU queue
+Used to for compring algorithm with and without CPU
+'''
+def assign_one_queue(task):
+    gpu_queue.append(task)
+# End assign_one_queue();
 
 '''
 Creates 100 random Jobs
@@ -58,12 +59,33 @@ Creates 100 random Jobs
 def generate_queue():
     queue = []
     for i in range(0,100):
-        job_size = randint(1,4)
-        global curr_id
-        new_task = Task(curr_id, JobSize(job_size))
+        job_size = randint(1,5)
+        global CURR_ID
+        new_task = Task(CURR_ID, JobSize(job_size))
         queue.append(new_task)
-        curr_id += 1
+        CURR_ID += 1
     return queue
+# End generate_queue();
+
+'''
+Looks at next task and assigns processor depending on JobSize and Allocated Time
+While also implementing a GERM scheduling policy
+'''
+def GERM_process_task_variant(task_queue):
+    task = task_queue.pop(0)
+    if task._size == JobSize.SMALL:
+        global cpu_queue
+        cpu_queue.append(task)
+        return
+    if task._size.value > task._allocated:
+        task._allocated += 2
+        task_queue.append(task)
+        return
+    else:
+        global gpu_queue
+        gpu_queue.append(task)
+    return
+# End process_task();
 
 '''
 Print both queues to the console
@@ -77,7 +99,7 @@ def print_queues():
     global cpu_queue
     for task in cpu_queue:
         print(str(task))
-
+# End print_queues();
 
 ################
 ####  Main  ####
@@ -86,13 +108,22 @@ def print_queues():
 random.seed(1)
 
 task_queue = generate_queue()
+print(len(task_queue))
 cpu_queue = []
 gpu_queue = []
 
-
-for job in task_queue:
-    assign_queue(job)
-print("*****Total Tasks*****")
-for task in task_queue:
-    print(str(task))
+while len(task_queue) > 0:
+    print(len(task_queue))
+    GERM_process_task_variant(task_queue)
 print_queues()
+print(len(cpu_queue))
+print(len(gpu_queue))
+'''Comments
+New schedule algorithm for OS managing Accelerator devices
+
+Need checker for payload > or < allotted
+Track time something gets in OS or GPU
+Track time it would have gotten done
+Track time it got done
+Jobs processed
+'''
